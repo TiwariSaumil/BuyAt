a9facdfe5c4d5e2683a5600e0de5026930fdefe4
Saumil Tiwari <saumiltiwari14@gmail.com>
2023-09-27 18:13:14 +0530

    first commit

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer footer-center p-4 border-t text-base-content">
+      <div>
+        <p>Copyright © 2023 - All right reserved by SAUMIL TIWARI</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
